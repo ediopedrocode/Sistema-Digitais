bec948906835ed56771cb87aa6014c7c6635aaf2
Édio Pedro <56984327+ediopedrocode@users.noreply.github.com>
2020-03-16 10:46:52 -0300

    Add files via upload

---
+++ Simulação 1: 12-02/ci7404.vhd
@@ -0,0 +1,22 @@
+-- Aula 12/02/2020 - Sistemas Digitais - 1°
+-- CÓDIGO 1 - PRINCIPAL
+-- Assunto: Introdução ao uso do Quartus
+-- Aluno: Édio Pedro
+------------------------------------------
+
+entity ci7404 is 
+port(A1,A2,A3,A4,A5,A6: in bit;
+	  Y1,Y2,Y3,Y4,Y5,Y6: out bit);
+end ci7404;
+
+architecture premero of ci7404 is
+begin
+	
+	Y1 <= not A1;
+	Y2 <= not A2;
+	Y3 <= not A3;
+	Y4 <= not A4;
+	Y5 <= not A5;
+	Y6 <= not A6;
+	
+end premero;
